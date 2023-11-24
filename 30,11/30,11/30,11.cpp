b8c4268463483aca050dcd4a4546e63cfcc60a17
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-11-24 23:58:36 +0200

    Add files via upload

---
+++ 30,11/30,11/30,11.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+
+int main() {
+    const int size = 5;
+    std::string surnames[size];
+
+    std::cout << "Введите фамилии 5 студентов:\n";
+    for (int i = 0; i < size; ++i) {
+        std::cout << "Фамилия " << i + 1 << ": ";
+        std::cin >> surnames[i];
+    }
+
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (surnames[j] > surnames[j + 1]) {
+                std::swap(surnames[j], surnames[j + 1]);
+            }
+        }
+    }
+
+    std::cout << "\nОтсортированные фамилии студентов:\n";
+    for (int i = 0; i < size; ++i) {
+        std::cout << i + 1 << ": " << surnames[i] << "\n";
+    }
+
+    return 0;
+}
